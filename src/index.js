8e3d5c9132bb2866536e104367e1fc87238e5d1b
igo-l <igolllf@gmail.com>
2021-08-23 23:11:00 +0300

    feat:  solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  for(let i = 0; i < str.length; i++){
+    for (let pairBracket of bracketsConfig) {
+      if (str[i] == pairBracket[0]  && pairBracket[0] == pairBracket[1]  && pairBracket[0] == stack[stack.length - 1]){
+        stack.pop();
+        break;
+        }
+        if (str[i] == pairBracket[0]){
+          stack.push(str[i]);
+          break;
+          }
+          if (str[i] == pairBracket[1]){
+            if(pairBracket[0] != stack.pop()){
+              return false;
+              }
+          }
+      }
+  }
+  if (stack.length) return false;
+  return true;
 }
