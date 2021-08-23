19a625c11d91229b59fbdc1bc5a4c9cd92d83128
igo-l <igolllf@gmail.com>
2021-08-23 23:22:57 +0300

    fix: correct

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
   let stack = [];
-  for(let i = 0; i < str.length; i++){
+  for (let i = 0; i < str.length; i++) {
     for (let pairBracket of bracketsConfig) {
-      if (str[i] == pairBracket[0]  && pairBracket[0] == pairBracket[1]  && pairBracket[0] == stack[stack.length - 1]){
+      if (str[i] == pairBracket[0] && pairBracket[0] == pairBracket[1] && pairBracket[0] == stack[stack.length - 1]) {
         stack.pop();
         break;
         }
-        if (str[i] == pairBracket[0]){
+        if (str[i] == pairBracket[0]) {
           stack.push(str[i]);
           break;
           }
-          if (str[i] == pairBracket[1]){
-            if(pairBracket[0] != stack.pop()){
+          if (str[i] == pairBracket[1]) {
+            if (pairBracket[0] != stack.pop()) {
               return false;
               }
           }
